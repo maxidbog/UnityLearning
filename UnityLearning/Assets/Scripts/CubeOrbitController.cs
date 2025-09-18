eaa316c061da9b742b35bdddbce0b19d5a7aab83
maxidbog <maxidbog@gmail.com>
2025-09-18 21:53:33 +0500

    Task2 last work

--- UnityLearning/Assets/Scripts/CubeOrbitController.cs
+++ UnityLearning/Assets/Scripts/CubeOrbitController.cs
@@ -8,11 +8,14 @@ public class CubeOrbitController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private float orbitRadius = 5f;
     [Tooltip("Об/Мин")][SerializeField] private float rotationSpeed = 1f;
+    [SerializeField] private bool isReversed = false;
     [SerializeField] private int cubeCount = 4;
     [SerializeField] private GameObject cubePrefab;
+    [SerializeField] private DistributionType distributionType;
+    [SerializeField] private int cubeInterval = 5;
 
-    private List<GameObject> orbitingCubes = new List<GameObject>();
-    private float rotationAngle;
+
+    //private List<GameObject> orbitingCubes = new List<GameObject>();
 
 
     private void Awake()
@@ -21,8 +24,8 @@ public class CubeOrbitController : MonoBehaviour
         {
             GameObject cube = Instantiate(cubePrefab, transform);
             cube.transform.parent = transform;
-            cube.transform.localPosition = CalculateOrbitPosition(i * 2 * Mathf.PI / cubeCount, orbitRadius);
-            orbitingCubes.Add(cube);
+            cube.transform.localPosition = CalculateOrbitPosition(GetDeviationAngle(i), orbitRadius);
+            //orbitingCubes.Add(cube);
         }
     }
 
@@ -39,7 +42,8 @@ public class CubeOrbitController : MonoBehaviour
 
     void FixedUpdate()
     {
-        rotationAngle = rotationSpeed * 360 / 60 / 50 * Mathf.Deg2Rad;
+        int rotationDirection = isReversed ? -1 : 1;
+        float rotationAngle = rotationSpeed * 360 / 60 / 50 * Mathf.Deg2Rad * rotationDirection;
         transform.RotateAroundLocal(new Vector3(0, 1, 0), rotationAngle);
     }
 
@@ -48,5 +52,21 @@ public class CubeOrbitController : MonoBehaviour
         float x = Mathf.Cos(angle) * radius;
         float z = Mathf.Sin(angle) * radius;
         return new Vector3(x, 0, z);
+    }
+
+    private float GetDeviationAngle(int i)
+    {
+        float deviationAngle = 0;
+        if (distributionType == DistributionType.Uniform)
+            deviationAngle = i * 2 * Mathf.PI / cubeCount;
+        else if (distributionType == DistributionType.Sequential)
+            deviationAngle = i * cubeInterval * 2 * Mathf.PI / 360;
+        return deviationAngle;
+    }
+
+    private enum DistributionType
+    {
+        Uniform,
+        Sequential
     }
 }
